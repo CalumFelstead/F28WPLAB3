b0d66167f5f6511219999e063d115363efd3097c
Calum Felstead <cf2007@hw.ac.uk>
2021-11-21 12:10:56 +0000

    Task 7-1-3.

--- db/productDAO.js
+++ db/productDAO.js
@@ -13,7 +13,7 @@ function findAll(callback) {
 }
 
 function findByID(reference, callback) {
-    const selectProducts = `SELECT * from article where reference like '08-151';`;
+    const selectProducts = `SELECT * from article where reference like '05-303';`;
     database.getResult(selectProducts, function(err, rows) {
         if (!err) {
             callback(null, rows);
